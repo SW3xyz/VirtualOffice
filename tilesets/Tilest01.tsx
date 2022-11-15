f545735da07292ccf0a60b7ef096a0d54a057edd
SW3xyz <alain@studioweb3.xyz>
2022-11-15 23:31:25 +0100

    Delete Tilest01.tsx

--- tilesets/Tilest01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Brainsonic_Workadventure_Export01_Layer02_Furniture" tilewidth="32" tileheight="32" tilecount="1421" columns="49">
- <image source="../../test/tilesets/Brainsonic_Workadventure_Export01_Layer02_Furniture.png" width="1568" height="928"/>
-</tileset>
