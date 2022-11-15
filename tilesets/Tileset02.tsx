ebc57754c25d1f230b15f2048e75a220ea041729
SW3xyz <alain@studioweb3.xyz>
2022-11-15 23:04:01 +0100

    Create Tileset02.tsx

---
+++ tilesets/Tileset02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Brainsonic_Workadventure_Export01_Layer03_AbovePlayer" tilewidth="32" tileheight="32" tilecount="1421" columns="49">
+ <image source="../../test/tilesets/Brainsonic_Workadventure_Export01_Layer03_AbovePlayer.png" width="1568" height="928"/>
+</tileset>
